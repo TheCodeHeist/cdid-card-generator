32050cc7c7286bef3acd63a0b06b2777ab04b5b4
Jokairo <jokairo@outlook.com>
2025-06-30 00:03:11 +0300

    Move index.html

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
         })
     ],
     html: {
-        template: './public/index.html',
+        template: './index.html',
     },
     output: {
         distPath: {
