3ed827123f48fe6a97772f7b91c51129c37888aa
Jokairo <jokairo@outlook.com>
2025-07-03 00:22:07 +0300

    Update export button, add better error messages

--- src/components/Export.tsx
+++ src/components/Export.tsx
@@ -1,6 +1,6 @@
 import { Button, Select, SimpleGrid, Stack } from "@mantine/core";
 import { toJpeg, toPng } from "html-to-image";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import Card from "./card/Card";
 import type { FormsData } from "../App";
 
@@ -29,44 +29,41 @@ const Export = ({ exporting, setExporting, formData }: ExportI) => {
 	const [times, setTimes] = useState(0);
 	const ref = useRef(null);
 
-	useEffect(() => {
-		const handleExport = async () => {
-			if (ref.current == null) {
-				if (times == 3) {
-					console.error("Failed to export: tried too many times")
-					alert("Error exporting image");
-					setTimes(0);
-					return;
-				}
-				setTimes((prev) => prev+1);
-				setTimeout(() => { handleExport() }, 2000);
+	const startExport = async () => {
+		if (exporting) return;
+
+		setExporting(true);
+
+		if (ref.current == null) {
+			if (times == 3) {
+				console.error("Failed to export: tried too many times")
+				alert("Error exporting image. Please try again or try smaller image resolution.");
+				setTimes(0);
 				return;
 			}
+			setTimes((prev) => prev+1);
+			setTimeout(() => { startExport() }, 2000);
+			return;
+		}
 
-			try {
-				const dataUrl = imageType == "png" ? await toPng(ref.current) : await toJpeg(ref.current, { quality: 90 });
-				const link = document.createElement("a");
-				link.download = `custom-card.${imageType}`;
-				link.href = dataUrl;
-				link.click();
-				setExporting(false);
-				setTimes(0);
-			}
-			catch (err) {
-				console.error("Failed to export:", err);
-				alert("Error exporting image");
-				setExporting(false);
-				setTimes(0);
-			}
-		};
-		if (exporting)
-			handleExport();
-
-	}, [exporting, times])
-
-	const startExport = () => {
-		setExporting(true);
+		try {
+			const dataUrl = imageType == "png" ? await toPng(ref.current) : await toJpeg(ref.current, { quality: 90 });
+			const link = document.createElement("a");
+			link.download = `custom-card.${imageType}`;
+			link.href = dataUrl;
+			link.click();
+			setExporting(false);
+			setTimes(0);
+		}
+		catch (err: any) {
+			console.error("Failed to export:", err);
+			alert(`Error exporting image. Please try again or try smaller image resolution.\n\nError message:\n${err.message}\n\nStack trace:\n${err.stack}`);
+			setExporting(false);
+			setTimes(0);
+		}
 	}
+
+	const buttonText = exporting ? "Please wait..." : `Download as ${imageType.toUpperCase()}`
 
 	return (
 		<>
@@ -77,16 +74,18 @@ const Export = ({ exporting, setExporting, formData }: ExportI) => {
 				value={scaleMultiplier}
 				onChange={(val) => setScaleMultiplier(val ?? "1")}
 				data={resolutionOptions}
+				disabled={exporting}
 			/>
 			<Select
 				label="Image Type"
 				value={imageType}
 				onChange={(val) => setImageType(val ?? "png")}
 				data={imageTypeOptions}
+				disabled={exporting}
 			/>
 			</SimpleGrid>
-			<Button onClick={startExport}>
-				Export as {imageType.toUpperCase()}
+			<Button onClick={startExport} disabled={exporting}>
+				{buttonText}
 			</Button>
 		</Stack>
 		<div
